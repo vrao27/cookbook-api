cecd524a8c55f6ead4a9412c1156d1841b62c0c2
Vinay Rao <vrao8912@gmail.com>
2022-11-21 17:42:28 +0100

    fetched data from contentful and stored

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fileSystem = require("fs");
 const axios = require("axios");
+const myRecipes = require("./myRecipes.json");
 
 //here we define an array with multiple recipes
-const recipes = [
-  { id: 1, name: "recipe1" },
-  { id: 2, name: "recipe2" },
-  { id: 3, name: "recipe3" },
-  { id: 4, name: "recipe4" },
-  { id: 5, name: "recipe5" },
-  { id: 6, name: "recipe6" },
-];
+// const recipes = [
+//   { id: 1, name: "recipe1" },
+//   { id: 2, name: "recipe2" },
+//   { id: 3, name: "recipe3" },
+//   { id: 4, name: "recipe4" },
+//   { id: 5, name: "recipe5" },
+//   { id: 6, name: "recipe6" },
+// ];
 
 //we pass a function with 2 arguments req and response
 
-// app.get("/", (req, res) => {
-//   res.send("Hey");
-// });
+app.get("/", (req, res) => {
+  //console.log(myRecipes);
+  res.json(myRecipes);
+});
 
 //Here we define another route to get all the recipes
 
-app.get("/api/recipes", (req, res) => {
-  res.send(recipes);
-});
+// app.get("/api/recipes", (req, res) => {
+//   res.send(recipes);
+// });
 
 //create a route woth a dynamic id to get single recipes
 //id here is the name of ther parameter, it can be named anything
@@ -33,11 +35,11 @@ app.get("/api/recipes", (req, res) => {
 
 //we parse the req.params.id into an integer as its original form is a string
 
-app.get("/api/recipes/:id", (req, res) => {
-  const recipe = recipes.find((r) => r.id === parseInt(req.params.id));
-  if (!recipe) res.status(404).send("Recipe not found");
-  res.send(recipe);
-});
+// app.get("/api/recipes/:id", (req, res) => {
+//   const recipe = recipes.find((r) => r.id === parseInt(req.params.id));
+//   if (!recipe) res.status(404).send("Recipe not found");
+//   res.send(recipe);
+// });
 
 //PORT is an env variable
 
