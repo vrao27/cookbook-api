79712285a65060debe1b3d0b35b64562ee0af5af
Vinay Rao <vrao8912@gmail.com>
2022-11-21 16:06:38 +0100

    added axios

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const fileSystem = require("fs");
+const axios = require("axios");
 
 //here we define an array with multiple recipes
 const recipes = [
-  { id: 1, recipeName: "recie1" },
-  { id: 2, recipeName: "recie2" },
-  { id: 3, recipeName: "recie3" },
-  { id: 4, recipeName: "recie4" },
-  { id: 4, recipeName: "recie5" },
-  { id: 6, recipeName: "recie6" },
+  { id: 1, name: "recipe1" },
+  { id: 2, name: "recipe2" },
+  { id: 3, name: "recipe3" },
+  { id: 4, name: "recipe4" },
+  { id: 5, name: "recipe5" },
+  { id: 6, name: "recipe6" },
 ];
 
 //we pass a function with 2 arguments req and response
 
-app.get("/", (req, res) => {
-  res.send("Hey");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hey");
+// });
 
 //Here we define another route to get all the recipes
 
@@ -27,9 +29,14 @@ app.get("/api/recipes", (req, res) => {
 //create a route woth a dynamic id to get single recipes
 //id here is the name of ther parameter, it can be named anything
 //we use req.query to call out a recipe based on its id value
+// use r as an argument to denote a recipe that we want to find based on its id
+
+//we parse the req.params.id into an integer as its original form is a string
 
 app.get("/api/recipes/:id", (req, res) => {
-  res.send(req.query);
+  const recipe = recipes.find((r) => r.id === parseInt(req.params.id));
+  if (!recipe) res.status(404).send("Recipe not found");
+  res.send(recipe);
 });
 
 //PORT is an env variable
