59e3b8f74b80dd8e3596d7662c201b6991fb7853
suqun zhang <susizhang2017@gmail.com>
2022-11-21 09:49:14 +0100

    Adding basic file

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-
-//we pass a function with 2 arguments req and response
-// app.get("/", (req, res) => {
-//   res.send("Hey");
-// });
-
-//Here we define another route
-app.get("/api/recipes", (req, res) => {
-  res.send([1, 2, 3]);
+const port = 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-//create a route woth a dynamic id to get recipes
-//id here is the name of ther parameter, it can be named anything
-app.get("/api/recipes/:id", (req, res) => {
-  res.send(req.params.id);
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
-
-//PORT is an env variable
-
-app.listen(port, () => console.log(`Listening on port ${port}...`));
