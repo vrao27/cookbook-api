0a9c8a8197546858d5943a0e8aa294e973d52950
Vinay Rao <vrao8912@gmail.com>
2022-11-21 14:24:04 +0100

    using req.query

--- app.js
+++ app.js
@@ -2,20 +2,34 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-//we pass a function with 2 arguments req and response
-// app.get("/", (req, res) => {
-//   res.send("Hey");
-// });
+//here we define an array with multiple recipes
+const recipes = [
+  { id: 1, recipeName: "recie1" },
+  { id: 2, recipeName: "recie2" },
+  { id: 3, recipeName: "recie3" },
+  { id: 4, recipeName: "recie4" },
+  { id: 4, recipeName: "recie5" },
+  { id: 6, recipeName: "recie6" },
+];
 
-//Here we define another route
-app.get("/api/recipes", (req, res) => {
-  res.send([1, 2, 3]);
+//we pass a function with 2 arguments req and response
+
+app.get("/", (req, res) => {
+  res.send("Hey");
 });
 
-//create a route woth a dynamic id to get recipes
+//Here we define another route to get all the recipes
+
+app.get("/api/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+//create a route woth a dynamic id to get single recipes
 //id here is the name of ther parameter, it can be named anything
+//we use req.query to call out a recipe based on its id value
+
 app.get("/api/recipes/:id", (req, res) => {
-  res.send(req.params.id);
+  res.send(req.query);
 });
 
 //PORT is an env variable
